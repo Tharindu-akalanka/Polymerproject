0c3218daafaba82d52c4de1448b33ed45eabe102
Tharindu Akalanka <tharinduakalanka930@gmail.com>
2026-04-30 00:35:53 +0530

    Update base URL for moved repository

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Polymer-project/',
+  base: '/Polymerproject/',
   plugins: [react()],
 })
